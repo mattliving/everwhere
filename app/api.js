397a75b9fa2119e821b4403db742a27c945a33a0
M L Livingston <matt.l.livingston@gmail.com>
2015-10-11 15:26:10 +0100

    Add scrapbook service

--- app/api.js
+++ app/api.js
@@ -1,6 +1,12 @@
+import config from 'config'
+import jsdom from 'jsdom'
 import rp from 'request-promise'
+import path from 'path'
+import url from 'url'
 import p from '../lib/p'
 import {convertPlace} from '../lib/helpers'
+
+let scrapbookEndpoint = config.services.scrapbook.endpoint
 
 export default function api (app) {
   app.get('/s/:place', p(function (req, res, next) {
@@ -17,13 +23,26 @@ export default function api (app) {
   }))
 
   app.get('/test', p(function (req, res, next) {
-    let uri = 'https://www.airbnb.co.uk/s/Kyoto--Japan?checkin=13-11-2015&checkout=23-11-2015&room_types%5B%5D=Entire+home%2Fapt&room_types%5B%5D=Private+room&price_min=13&price_max=87&ib=true&zoom=12&search_by_map=true&sw_lat=34.90532812884305&sw_lng=135.67134002954526&ne_lat=35.08266908600762&ne_lng=135.855361025639&ss_id=gcop68ot'
+    // let uri = 'https://www.airbnb.co.uk/s/Kyoto--Japan?checkin=13-11-2015&checkout=23-11-2015&room_types%5B%5D=Entire+home%2Fapt&room_types%5B%5D=Private+room&price_min=13&price_max=87&ib=true&zoom=12&search_by_map=true&sw_lat=34.90532812884305&sw_lng=135.67134002954526&ne_lat=35.08266908600762&ne_lng=135.855361025639&ss_id=gcop68ot'
+    // return new Promise((resolve) => {
+    //   jsdom.env(
+    //     uri,
+    //     ['http://code.jquery.com/jquery.js'],
+    //     function (err, window) {
+    //       var $ = window.jQuery
+    //       console.log($.fn.jquery)
+
+    //       console.log($('a').length + ' links on the page!')
+    //       resolve($('body').length)
+    //     }
+    //   )
+    // })
+    let uri = `${url.resolve(scrapbookEndpoint, path.join(config.env, 'greetings', 'hello'))}`
     return rp.get({
-      uri,
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36'
-      }
-    })
+      uri: `${url.resolve(scrapbookEndpoint, path.join(config.env, 'greetings', 'hello'))}`,
+      json: true
+    }).then((val) => Promise.resolve(val))
+      .catch(e => Promise.reject(e))
   }))
 
   return app
